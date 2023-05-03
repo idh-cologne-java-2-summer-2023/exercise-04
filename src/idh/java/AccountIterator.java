b1e11ef161954d15014e3f80549893b440357c57
JohannonBlake <johannon.blade@Gmail.com>
2023-05-03 15:01:20 +0200

    just got started

---
+++ src/idh/java/AccountIterator.java
@@ -0,0 +1,20 @@
+package idh.java;
+
+import java.util.Iterator;
+
+public class AccountIterator implements Iterator<Account> {
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasNext'");
+    }
+
+    @Override
+    public Account next() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'next'");
+    }
+    
+    
+}
