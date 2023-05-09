652e86cb9fa7e036c659d8cc582455d13e0f7441
Mecki <maximue98@gmail.com>
2023-05-09 12:28:31 +0200

    Update zu eigenem Code

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -1,94 +1,148 @@
-package idh.java;
+package b;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Random;
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ATM {
-	
-	// initial cash in the ATM
-	int cash = 100;
+	List<Bankaccount> bankaccounts = new ArrayList<>();
+	amountATM atmFortune = new amountATM(1000);
+	public class amountATM {
+		double cashATM = 0;
+		public amountATM(double cashATM) {
+			this.cashATM = cashATM;
+		}
 
-	// accounts known to the ATM
-	Account[] accounts = new Account[5];
+		public double getCashATM() {
+			return cashATM;
+		}
 
-	public ATM() {
-		// create accounts with varying balances
-		Random random = new Random();
-		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
+		public void setCashATM(double cashATM) {
+            this.cashATM = cashATM;
+        }
+	}
+
+	public class Bankaccount {
+		double balance = 0;
+		public Bankaccount(double balance, Account account) {
+			this.balance = balance;
+			this.account = account;
+		}
+
+		Account account;
+
+		public double getBalance() {
+			return balance;
+		}
+
+		public void setBalance(double balance) {
+            this.balance = balance;
+        }
+	}
+
+	// double hundert = 100; // TODO: zum Objekt machen
+
+	public class Account {
+
+		int id;
+
+		public Account(int id) {
+			this.id = id;
+		}
+
+		public int getId() {
+			return id;
 		}
 	}
-	
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
-	public void run() {
+
+	public void runID() {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			try {
+				System.out.print("Enter your bank's ID: ");
+				String input = reader.readLine();
+				int id = Integer.parseInt(input); //int ID = das was eingegeben wurde in int
+				if (input.matches("\\d+")) {
+
+					Bankaccount currentBankaccount = null;
+
+					boolean accountExists = false;
+					for (Bankaccount bankaccount : bankaccounts) {
+						if (bankaccount.account.id == id) {
+							accountExists = true;
+							currentBankaccount = bankaccount;
+							break;
+						}
+					}
+					if (!accountExists) {
+						Account account = new Account(id);
+						Bankaccount bankaccount = new Bankaccount(100, account); //hier muss Bankaccount ( getID(), 100) stehen und Bankaccount muss mit Account zu einer einzigen Methode zusammengeführt werden
+						bankaccounts.add(bankaccount);
+						currentBankaccount = bankaccount;
+					}
+
+					System.out.println("Your bank's ID is: " + currentBankaccount.account.getId() + " and your balance is "
+							+ currentBankaccount.getBalance() + "€");
+					withdraw(currentBankaccount, atmFortune);
+				}
+			} catch (Exception e) {
+				System.out.println("This ID doesn't exist.");
+				break;
+			}
+		}
+
+	}
+
+	public void withdraw(Bankaccount bankaccount, amountATM atmFortune) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
-				System.out.print("Enter your account number: ");
-				int accountNumber = Integer.parseInt(br.readLine());
 				System.out.print("Enter the amount to withdraw: ");
-				int amount = Integer.parseInt(br.readLine());
-				cashout(accountNumber, amount);
+				double amount = Double.parseDouble(br.readLine()); // das gleiche nur mit double, um Kommazahlen darzustellen
+				cashout(amount, bankaccount, atmFortune); //kommt cashATM hier rein?
+				runID();
 			} catch (Exception e) {
-				e.printStackTrace();
 				break;
 			}
 		}
 	}
 
-	public void cashout(int accountNumber, int amount) {
-		// check for cash in the ATM
-		if (amount > cash) {
-			System.out.println("Sorry, not enough cash left.");
-			return;
-		}
-		
-		// check for existence of the account
-		Account account = getAccount(accountNumber);
-		if (account == null) {
-			System.out.println("Sorry, this account doesn't exist.");
-			return;
-		}
-		
-		// check for balance of the account
-		if (amount > account.getBalance()) {
-			System.out.println("Sorry, you're out of money.");
-			return;
-		}
-		
-		// withdraw
-		account.withdraw(amount);
-		cash += amount;
-		System.out.println("Ok, here is your money, enjoy!");
+	public void cashout(double amount, Bankaccount bankaccount, amountATM atmFortune) {
+		String amountString = Double.toString(amount);
+		double moneymoney = bankaccount.getBalance();
+		double cashMoney = atmFortune.getCashATM();
+		if (moneymoney - amount >= 0 && cashMoney - amount >= 0 && amountString.matches("^\\d+(\\.\\d{1,2})?$")) {
 
-	};
+			/*
+			 * \d+ -> Übereinstimmung mit einer oder mehrerer Ziffern, in dem Fall Positive
+			 * Dezimalzahl
+			 * (\.\d{1,2})? -> Optionale Gruppe. Zahl kann einen Punkt mit 1 oder 2
+			 * Nachkommastellen haben.
+			 * ? -> Optionale Gruppe.
+			 * $ -> beendet
+			 */
 
-	/**
-	 * Launches the ATM
-	 */
-	public static void main(String[] args) {
-		ATM atm = new ATM();
-		atm.run();
-	};
-	
-	/**
-	 * Retrieves the account given an id.
-	 * 
-	 * @param id
-	 * @return
-	 */
-	protected Account getAccount(int id) {
-		for (Account account : accounts) {
-			if (account.getId() == id) 
-				return account;
+			// TODO: Math.abs(amount - Math.floor(amount)) > 0.01 || amount == (int)amount)
+			// {
+
+			// amount = Math.abs(amount); // sorgt dafür, dass der ausgegebene Betrag immer
+			// positiv ist, ist aber anscheinend überflüssig, wegen des "d+" von oben aus
+			// der if-Bedingung
+			moneymoney = moneymoney - amount;
+			cashMoney = cashMoney - amount;
+			System.out.println("You withdrawed " + amount + ". Your account balance is now " + moneymoney + "€");
+			bankaccount.setBalance(moneymoney);
+			atmFortune.setCashATM(cashMoney);
+			System.out.println("The ATMs cash stash is " + cashMoney + "€");
+		} else {
+			System.err.println("This action does not work. Your account balance is " + moneymoney + "€. The ATMs money stock is at " + cashMoney + "€."); // sollte dieser Teil in die Exception?
 		}
-		return null;
 	}
 
+	// Launches the ATM
+	public static void main(String[] args) {
+		ATM atm = new ATM();
+		atm.runID();
+		// atm.run();
+	}
 }
