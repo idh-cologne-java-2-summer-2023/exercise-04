412a1ac94eec5ff1741972f9982745a8703e2cab
sophi <sophi@klick-klick-klick-tipp-tipp.fritz.box>
2023-05-10 08:43:35 +0200

    Beginn Aufgabe 1. Schwenke weiße Fahne

--- src/idh/java/Account.java
+++ src/idh/java/Account.java
@@ -2,21 +2,23 @@ package idh.java;
 
 /**
  * This class represents an account in our bank.
+ * 
  * @author reiterns
  *
  */
 public class Account {
 	// the balance of the account
 	int balance;
-	
+
 	// the id of the account
 	int id;
 
 	public Account(int id, int status) {
+		super();
 		this.id = id;
 		this.balance = status;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
@@ -33,13 +35,15 @@ public class Account {
 		this.balance = status;
 	}
 	
+
 	/**
 	 * Withdraws a sum of money from the account
+	 * 
 	 * @param sum
 	 */
 	public void withdraw(int sum) {
 		this.balance = balance - sum;
 	}
-	
-	
+
+
 }
